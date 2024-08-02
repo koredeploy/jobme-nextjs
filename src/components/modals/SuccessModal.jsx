562b92ef896756bf9d748669d093889e22c58f76
Daniel <danielkore1234@gmail.com>
2024-08-02 05:53:55 +0100

    updated style

--- src/components/modals/SuccessModal.jsx
+++ src/components/modals/SuccessModal.jsx
@@ -20,10 +20,10 @@ export default function SuccessModal({ show, setShow, headerText, path }) {
       />
 
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <div className="flex min-h-full  justify-center p-4 items-center text-center sm:items-center sm:p-0">
           <DialogPanel
             transition
-            className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg  lg:w-96 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+            className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in w-full sm:my-8 sm:w-full sm:max-w-lg  lg:w-[400px] data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
           >
             <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4 ">
               <div className="sm:flex sm:items-start">
